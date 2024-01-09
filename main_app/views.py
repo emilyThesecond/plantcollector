d1a9a949c247e9e032bb120907172352b26847e1
emilyTheseond <esegundo02@gmail.com>
2024-01-09 12:40:28 -0600

    part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 plants = [
-    {'name': 'Monstera Deliciosa', 'common name': 'swiss cheese plant', 'family': 'Araceae'},
-    {'name': 'Ficus benjamin','common name': 'Weeping Fig','family': 'Moraceae'},
-    {'name': 'Ficus Lyrata','common name': 'Fiddle-Leaf Fig','family': 'Moraceae'}
+    {'name': 'Monstera Deliciosa', 'common_name': 'swiss cheese plant', 'family': 'Araceae'},
+    {'name': 'Ficus benjamin','common_name': 'Weeping Fig','family': 'Moraceae'},
+    {'name': 'Ficus Lyrata','common_name': 'Fiddle-Leaf Fig','family': 'Moraceae'}
 ]
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def plants_index(request):
+    return render(request, 'plants/index.html', {'plants' : plants})
