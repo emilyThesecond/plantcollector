ec74c44c01af411588f7767514a2b26fa14cdb76
emilyTheseond <esegundo02@gmail.com>
2024-01-09 10:56:28 -0600

    start

---
+++ main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+plants = [
+    {'name': 'Monstera Deliciosa', 'common name': 'swiss cheese plant', 'family': 'Araceae'},
+    {'name': 'Ficus benjamin','common name': 'Weeping Fig','family': 'Moraceae'},
+    {'name': 'Ficus Lyrata','common name': 'Fiddle-Leaf Fig','family': 'Moraceae'}
+]
+
+# Create your views here.
